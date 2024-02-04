b7134bbe63310b7894c796180a50f79af99c645e
dylan <justbeawesome9@gmail.com>
2024-02-04 14:09:25 -0500

    feat: point rendering

--- js/display.js
+++ js/display.js
@@ -12,14 +12,23 @@ export default class Display {
 
     animate() {
         requestAnimationFrame(this.animate.bind(this));
-
         const points = this.board.points
+        const gridWrap = document.getElementById("pvOut")
 
         this.ctx.clearRect(0,0, this.experience.sizes.width, this.experience.sizes.height)
+        gridWrap.innerHTML = ""
 
         for (let i = 0; i < points.length; i++) {
             const x = (points[i].x/this.board.width)*this.experience.sizes.width;
             const y = (points[i].y/this.board.height)*this.experience.sizes.height;
+
+            const newPoint = document.createElement("div")
+            newPoint.classList.add("point")
+            newPoint.style.left = `${x}px`
+            newPoint.style.top = `${y}px`
+            newPoint.innerHTML = i+1;
+            gridWrap.appendChild(newPoint)
+
             if (i > 0) {
                 const x2 = (points[i-1].x/this.board.width)*this.experience.sizes.width;
                 const y2 = (points[i-1].y/this.board.height)*this.experience.sizes.height;
